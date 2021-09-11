ea6eef0f50aa7341bc734001a6bb98d9cf7d172d
ProjectWeb <thuantmse150124@fpt.edu.vn>
2021-09-11 23:00:34 +0700

    Upload - Update Password & User Info

--- Services/Interface/IUserService.cs
+++ Services/Interface/IUserService.cs
@@ -1,8 +1,12 @@
+using backend.Models;
+using backend.Controllers.DTO;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
 
 namespace backend.Services.Interface
 {
     public interface IUserService
     {
-
+        public bool updatePasswordHandler(UpdatePasswordDTO input, ViewDataDictionary dataView);
+        public bool updateUserInfoHandler(UpdateUserInfoDTO input, ViewDataDictionary dataView);
     }
 }
--- Services/UserService.cs
+++ Services/UserService.cs
@@ -9,6 +9,7 @@ using backend.DAO.Interface;
 using backend.Utils.Locale;
 using backend.Models;
 using backend.Services.Interface;
+using backend.Utils.Interface;
 
 namespace backend.Services
 {
@@ -16,13 +17,58 @@ namespace backend.Services
     {
         private readonly DBContext dbContext;
         private readonly IUserRepository userRepository;
-        public UserService(DBContext dbContext, IUserRepository userRepository)
+        private readonly IAuthService authService;
+
+        public UserService(DBContext dbContext, IUserRepository userRepository, IAuthService authService)
         {
             this.dbContext = dbContext;
             this.userRepository = userRepository;
+            this.authService = authService;
         }
 
+        public bool updatePasswordHandler(UpdatePasswordDTO input, ViewDataDictionary dataView)
+        {
+            ValidationResult result = new UpdatePasswordDTOValidator().Validate(input);
+            if (!result.IsValid)
+            {
+                ServerResponse.mapDetails(result, dataView);
+                return false;
+            }
+            Console.WriteLine(input.oldPassword);
+            User user = (User)dataView["user"];
 
+            if (!authService.comparePassword(input.oldPassword, user.password))
+            {
+                ServerResponse.setErrorMessage(CustomLanguageValidator.ErrorMessageKey.ERROR_OLD_PASSWORD_NOT_CORRECT, dataView);
+                return false;
+            }
 
+            user.password = authService.hashingPassword(input.newPassword);
+            Console.WriteLine(user.password);
+
+            this.dbContext.user.Update(user);
+            this.dbContext.SaveChanges();
+            return true;
+        }
+
+        public bool updateUserInfoHandler(UpdateUserInfoDTO input, ViewDataDictionary dataView)
+        {
+            ValidationResult result = new UpdateUserInfoDTOValidator().Validate(input);
+            if (!result.IsValid)
+            {
+                ServerResponse.mapDetails(result, dataView);
+                return false;
+            }
+
+            User user = (User)dataView["user"];
+            user.name = input.name;
+            user.email = input.email;
+            user.phone = input.phone;
+            user.address = input.address;
+
+            this.dbContext.Update(user);
+            this.dbContext.SaveChanges();
+            return true;
+        }
     }
 }
