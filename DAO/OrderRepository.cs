9a1af6024cd9e53a6a940b5884f1aa6a0763a688
thaithinh <thaithinhbanzai79@gmail.com>
2021-09-17 11:04:00 +0700

    FIX - OrderModule

--- DAO/OrderRepository.cs
+++ DAO/OrderRepository.cs
@@ -25,6 +25,11 @@ namespace Backend.DAO
         public List<OrderItem> GetOrderDetail(string orderId)
         {
             List<OrderItem> orderItems = this.DBContext.OrderItem.Where(x => x.OrderId == orderId).ToList();
+            foreach (var orderItem in orderItems)
+            {
+                this.DBContext.Entry(orderItem).Reference(item => item.Product).Load();
+                this.DBContext.Entry(orderItem.Product).Reference(item => item.Category).Load();
+            }
             return orderItems;
         }
 
@@ -36,6 +41,11 @@ namespace Backend.DAO
 
         public List<Order> SearchOrders(string startDate, string endDate, string search)
         {
+            if (search == null)
+            {
+                search = "";
+            }
+            search = search.ToLower();
             List<string> stringDate = new List<string>();
             stringDate = startDate.Split('-').ToList();
             startDate = stringDate[1] + "/" + stringDate[2] + "/" + stringDate[0];
@@ -43,12 +53,16 @@ namespace Backend.DAO
             endDate = stringDate[1] + "/" + stringDate[2] + "/" + stringDate[0];
             DateTime sDate = Convert.ToDateTime(startDate);
             DateTime eDate = Convert.ToDateTime(endDate);
-            List<Order> orders = this.DBContext.Order.Where(x => x.Customer.Name.Contains(search) || x.Customer.Phone.Contains(search) || x.Customer.Email.Contains(search)).ToList();
+            List<Order> orders = this.DBContext.Order.ToList();
+            foreach (var order in orders)
+            {
+                this.DBContext.Entry(order).Reference(item => item.Customer).Load();
+            }
 
             for (int i = orders.Count - 1; i >= 0; i--)
             {
                 DateTime date = Convert.ToDateTime(orders[i].CreateDate);
-                if (DateTime.Compare(date, eDate) > 0 || DateTime.Compare(date, sDate) < 0)
+                if (DateTime.Compare(date, eDate) > 0 || DateTime.Compare(date, sDate) < 0 || (!orders[i].Customer.Name.ToLower().Contains(search) && !orders[i].Customer.Email.Contains(search) && !orders[i].Customer.Phone.Contains(search)))
                 {
                     orders.RemoveAt(i);
                 }
